4a339ced5708f9e54daaa2bdc0cc0f63f9ef62ce
Arthur Loginow <loginowarthur@gmail.com>
2021-03-15 20:36:58 -0300

    create new ng app

---
+++ ng-weather/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {}
