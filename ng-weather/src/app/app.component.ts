4809190bb2ec59b9d45fb01aa793be490ee83545
Arthur Loginow <loginowarthur@gmail.com>
2021-03-15 22:15:53 -0300

    add basic components

--- ng-weather/src/app/app.component.ts
+++ ng-weather/src/app/app.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'nw-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private weatherBaseUrl = 'http://api.weatherstack.com/';
+  private accessKey = 'd2a9d12632711fd00979308c4f30b68d';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get(
+        `${this.weatherBaseUrl}current?access_key=${this.accessKey}&query=10001`
+      )
+      .subscribe(console.log);
+  }
+}
--- ng-weather/src/app/app.module.ts
+++ ng-weather/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { WeatherListComponent } from './components/weather-list/weather-list.component';
+import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SearchBarComponent, WeatherListComponent, WeatherDetailsComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
